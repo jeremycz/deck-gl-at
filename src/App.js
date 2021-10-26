598073a3954766131fa48bd6c2adf6568193e552
Jeremy Chen <jeremy.cz@gmail.com>
2021-10-27 00:45:47 +1300

    Initial map render

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import DeckGL from '@deck.gl/react';
+import { LineLayer } from '@deck.gl/layers';
+import { StaticMap } from 'react-map-gl';
+
+// Viewport settings
+const INITIAL_VIEW_STATE = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0,
+};
+
+// Data to be used by the LineLayer
+const data = [
+  {
+    sourcePosition: [-122.41669, 37.7853],
+    targetPosition: [-122.41669, 37.781],
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DeckGL initialViewState={INITIAL_VIEW_STATE} controller>
+      <LineLayer id="line-layer" data={data} />
+      <StaticMap mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API_TOKEN} />
+    </DeckGL>
   );
 }
 
